c4ae79c935dd0ea326d2830442adce4ef909a58a
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2024-11-19 23:57:54 -0500

    Declare colors for light and dark theme

--- app/src/main/java/com/brightbox/hourglass/ui/theme/Color.kt
+++ app/src/main/java/com/brightbox/hourglass/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.brightbox.hourglass.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Bone = Color(0xFFDDDAD0)
+val BoneDarker = Color(0xFFBEB9AD)
+val White = Color(0xFFC8C6C6)
+val Ocean = Color(0xFF097C9C)
+val OceanDarker = Color(0xFF438298)
+val Blue = Color(0xFF01437C)
+val BlueDarker = Color(0xFF366298)
+val Gray = Color(0xFF3C3B37)
+val BlackSemiTransparent = Color(0x80000000) // Note: Adjusted to 50% transparency
+val Black = Color(0xFF000000)
+val LightGray = Color(0xFF959090)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green = Color(0xFF73A44A)
+val LightGreen = Color(0xFF6B9249)
+val Yellow = Color(0xFFE9A91F)
+val LightYellow = Color(0xFFB89040)
+val Red = Color(0xFFD33723)
+val LightRed = Color(0xFFB05861)
+val LightBlue = Color(0xFFA6ABB5)
--- app/src/main/java/com/brightbox/hourglass/ui/theme/Theme.kt
+++ app/src/main/java/com/brightbox/hourglass/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.brightbox.hourglass.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = BoneDarker,
+    onBackground = Gray,
+    surface = Bone,
+    onSurface = Gray,
+    surfaceVariant = LightGray,
+    primary = Blue,
+    onPrimary = White,
+    secondary = Ocean,
+    onSecondary = White,
+    error = Red,
+    onError = White,
 )
 
+
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = Black,
+    onBackground = White,
+    surface = LightBlue,
+    onSurface = Black,
+    surfaceVariant = BlackSemiTransparent,
+    primary = BlueDarker,
+    onPrimary = White,
+    secondary = OceanDarker,
+    onSecondary = Black,
+    error = LightRed,
+    onError = White,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
