b9b4f23999923ebc2c92da8d5abf1de119f40a38
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2024-11-27 14:05:29 -0500

    Change main activity to start at Home

--- app/src/main/java/com/brightbox/hourglass/MainActivity.kt
+++ app/src/main/java/com/brightbox/hourglass/MainActivity.kt
@@ -7,16 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import com.brightbox.hourglass.ui.theme.HourglassProductivityLauncherTheme
 import com.brightbox.hourglass.view.AppMenu
+import com.brightbox.hourglass.view.Home
 import com.brightbox.hourglass.viewmodel.AppsMenuViewModel
+import com.brightbox.hourglass.viewmodel.HomeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val appsMenuViewModel: AppsMenuViewModel by viewModels()
+        val homeViewModel: HomeViewModel by viewModels()
         setContent {
             HourglassProductivityLauncherTheme() {
-                AppMenu(appsMenuViewModel)
+                Home(homeViewModel)
             }
         }
     }
